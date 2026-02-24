9fdd744eda81275bd77c8a5d24872e1f630befff
vltrsrc-RC <vltrsrc@gmail.com>
2026-02-24 19:54:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from flask import Flask
+import functions_framework
+from google.cloud import storage
+import pandas as pd
+import io
 
-app = Flask(__name__)
+@functions_framework.cloud_event
+def converter_xlsx_para_csv(cloud_event):
+    data = cloud_event.data
+    bucket_name = data["bucket"]
+    file_name = data["name"]
 
-@app.route("/")
-def home():
-    return "API voluntarios funcionando"
+    if not file_name.startswith("entrada/horas/"):
+        return
+
+    if not file_name.endswith(".xlsx"):
+        return
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    content = blob.download_as_bytes()
+
+    df = pd.read_excel(
+        io.BytesIO(content),
+        sheet_name="Listagem de Horas",
+        skiprows=11
+    )
+
+    df = df.dropna(how="all")
+
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False, sep=";")
+
+    novo_nome = file_name.replace("entrada/", "processados/").replace(".xlsx", ".csv")
+
+    novo_blob = bucket.blob(novo_nome)
+    novo_blob.upload_from_string(csv_buffer.getvalue(), content_type="text/csv")
+
+    print(f"Convertido: {novo_nome}")
